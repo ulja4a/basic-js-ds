e879ac7496a48329a16d59d180314dc7cf9cbecb
ulja4a <ulja4a@gmail.com>
2023-04-17 12:24:10 +0400

    docs: passing tests

--- src/stack.js
+++ src/stack.js
@@ -12,20 +12,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
+
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+  this.arr = [];
+  }
+  push(element) {
+    return this.arr.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr[this.arr.length-1];
   }
 }
 
